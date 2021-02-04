12862b3fab2b78b6a69118e4e602c760de32b248
kirbycreese <70101203+kirbycreese@users.noreply.github.com>
2021-02-04 18:28:11 -0500

    working on button functionality

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import AnswerForm from './components/AnswerForm.js'
 import QuestionTable from './components/QuestionTable.js'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   render() {
--- client/src/components/QuestionTable.js
+++ client/src/components/QuestionTable.js
@@ -1,11 +1,62 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+// import Button from 'react-bootstrap/Button';
+
+import { Button } from 'react-bootstrap';
 
 export class QuestionTable extends Component {
     render() {
-        return (
-            <div>
-                this is where the question table will go
-            </div>
+         return (
+           <div>
+             <div>Jeopardy</div>
+             <table>
+             <thead>
+              <tr>
+               <th>Theatre Lingo</th>
+               <th>Name the Show</th>
+               <th>Who Wrote It</th>
+               <th>Starts With the Letter ''</th>
+               <th>One More Category</th>
+              </tr>
+            </thead>
+            <tbody>
+      <tr>
+        <td><Button variant="outline-primary">$200</Button>{' '}</td>
+        <td>$200</td>
+        <td>$200</td>
+        <td>$200</td>
+        <td>$200</td>
+      </tr>
+      <tr>
+        <td>$400</td>
+        <td>$400</td>
+        <td>$400</td>
+        <td>$400</td>
+        <td>$400</td>
+      </tr>
+      <tr>
+        <td>$600</td>
+        <td>$600</td>
+        <td>$600</td>
+        <td>$600</td>
+        <td>$600</td>
+      </tr>
+      <tr>
+        <td>$800</td>
+        <td>$800</td>
+        <td>$800</td>
+        <td>$800</td>
+        <td>$800</td>
+      </tr>
+      <tr>
+        <td>$1000</td>
+        <td>$1000</td>
+        <td>$1000</td>
+        <td>$1000</td>
+        <td>$1000</td>
+      </tr>
+    </tbody>
+             </table>
+           </div>
         )
     }
 }
