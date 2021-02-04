309112d3a673291b7989adf1f873d29a229bbe3d
RFFRYE <rffrye@users.noreply.github.com>
2021-02-04 12:17:56 -0500

    started Main page and json file of lyrics.

---
+++ client/src/pages /MainPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function MainPage() {
+        return (
+            <div>
+                <NavBar/>
+                <ContentArea/>
+                <Sidebar/>
+    
+            </div>
+        );
+};
+
+
+export default MainPage;
