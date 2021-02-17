05b0c7e187b700c2171132f3456f30033b8098d7
Richard Walter <richardwalter515@gmail.com>
2021-02-16 21:35:55 -0500

    created quizComplete js component

--- client/src/components/Quizzes/Wicked.js
+++ client/src/components/Quizzes/Wicked.js
@@ -1,23 +1,46 @@
-import React, { useContext } from "react"
+import React, { useContext, useState } from "react"
 import Quiz from 'react-quiz-component';
 import { ProfileContext } from "../../utils/GlobalState";
+import QuizComplete from './quizComplete';
+import wickedPlaybill from "../../imgs/playbills/wicked.png";
 
-const wickedData = require("./WickedData.json")
+const wickedData = require("./WickedData.json");
 
-const Wicked = () => {
+
+function Wicked(props) {
     let profile = useContext(ProfileContext);
+
+    const [showInfo, setShowInfo] = useState({
+        showName: "Wicked",
+        quizPointsEarned: 0,
+        showImage: wickedPlaybill,
+    });
     
     const onCompleteAction = (obj) => {
         let pointsEarned = obj.correctPoints;
         profile.dispatch({type: "addPoints", value: pointsEarned});
+        setShowInfo({...showInfo, quizPointsEarned: pointsEarned})
     
       };
+    if (showInfo.quizPointsEarned === 0) {
+        return (
+            <div>
+            <Quiz quiz = {wickedData} shuffle={true} showInstantFeedback={false} onComplete={onCompleteAction}/>
+            </div>
+        )
+    } else {
+        return (
+            <div>
+            <QuizComplete
+                name = {profile.ProfileAttributes.name}
+                showName = {showInfo.showName}
+                quizPointsEarned = {showInfo.quizPointsEarned}
+                showImage = {showInfo.showImage}
+             />
+            </div>
+        )
+    }
 
-    return (
-        <div>
-        <Quiz quiz = {wickedData} shuffle={true} showInstantFeedback={false} onComplete={onCompleteAction}/>
-        </div>
-    )
 }
 
 export default Wicked;
