62acc89e7c10ba2d5af0cc61382cb22b95317d05
Richard Walter <richardwalter515@gmail.com>
2021-02-18 20:28:09 -0500

    updated quizComplete.js styling and linked all quizzes

--- client/src/components/Quizzes/Heathers.js
+++ client/src/components/Quizzes/Heathers.js
@@ -1,25 +1,45 @@
-import React, { useContext } from "react"
+import React, { useContext, useState } from "react"
 import Quiz from 'react-quiz-component';
 import AppNav from "../Navbar";
+
 import { ProfileContext } from "../../utils/GlobalState";
+import QuizComplete from './quizComplete';
+import heathersPlaybill from "../../imgs/playbills/heathers.png";
 
 const heathersData = require("./HeathersData.json")
 
-const Heathers = () => {
+function Heathers() {
     let profile = useContext(ProfileContext);
+
+    const [showInfo, setShowInfo] = useState({
+        showName: "Heathers",
+        quizPointsEarned: 0,
+        showImage: heathersPlaybill,
+    });
     
     const onCompleteAction = (obj) => {
         let pointsEarned = obj.correctPoints;
         profile.dispatch({type: "addPoints", value: pointsEarned});
+        setShowInfo({...showInfo, quizPointsEarned: pointsEarned})
     
       };
-
-    return (
-        <div>
-        <AppNav />
-        <Quiz quiz = {heathersData} shuffle={true} showInstantFeedback={false} onComplete={onCompleteAction}/>
-        </div>
-    )
+    if (showInfo.quizPointsEarned === 0) {
+        return (
+            <div>
+            <Quiz quiz = {heathersData} shuffle={true} showInstantFeedback={false} onComplete={onCompleteAction}/>
+            </div>
+        )
+    } else {
+        return (
+            <div>
+            <QuizComplete
+                name = {profile.ProfileAttributes.name}
+                showName = {showInfo.showName}
+                quizPointsEarned = {showInfo.quizPointsEarned}
+                showImage = {showInfo.showImage}
+             />
+            </div>
+        )
+    }
 }
-
 export default Heathers;
